0b0fec4be1485fc0c6943848f4710df5f842e281
vyphotphet100 <caovy2001@gmail.com>
2021-10-17 17:55:41 +0700

    first commit

--- src/main/java/com/group3/shoesshop/repository/ProductRepository.java
+++ src/main/java/com/group3/shoesshop/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.group3.shoesshop.repository;
 
+import com.group3.shoesshop.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<ProductRepository, String> {
+public interface ProductRepository extends JpaRepository<ProductEntity, String> {
 }
