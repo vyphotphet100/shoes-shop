583a5a82f0d61b0c22b9badd5b1ca798a883ae1d
Hoàn Nguyễn <softteam2021@gmail.com>
2021-11-02 20:54:04 +0700

    add Admin UI for product

---
+++ src/main/resources/ui/Admin_Page/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $("#footer").before(`
+    <div style="margin-bottom: -20px; font-size: 12px; border-top: 1px solid #777777;" class="footer__container row">
+    <div class="item1 col">
+        <p>Noriva Admin — eCommerce Admin Page © 2021</p>
+    </div>
+    <div class="item2 col">
+        <p class="float-end">Created by G3 eCommerce —  All rights serves</p>
+    </div>
+</div>
+
+    `)
+})
